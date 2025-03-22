07837d0633c0b5e561996d95f1a7342b2f72cf67
ramneek <ramneeksingh963@gmail.com>
2025-03-23 03:34:16 +0530

    backend

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,9 @@ import { Button } from "@mui/material";
 
 const ImportButton = () => {
   const [result, setresult] = useState(null);
+  const [bollywoodData, setBollywoodData] = useState(null);
   const getData = async () => {
+    setBollywoodData(null)
     try {
       const response = await fetch("http://localhost:8000/api/data");
       if (!response.ok) {
@@ -15,14 +17,35 @@ const ImportButton = () => {
       console.log(err);
     }
   };
-
+  const getBollywoodData = async () =>{
+    setresult(null)
+    try{
+      const response =  await fetch("http://localhost:8000/api/bollywooddata")
+      if(!response.ok){
+        throw new Error("failed to fetch Bollywood Data")
+      }
+      const result = await response.json()
+      console.log(result)
+      setBollywoodData(result)
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div>
-      <Button onClick={getData}>Hello</Button>
+      <Button onClick={getData}>Hollywood</Button>
+      <Button onClick={getBollywoodData}>Bollyywood</Button>
       {result?.map((item) => {
         return (
-          <ul key={item.id}>
-            <li>{item.first_name}</li>
+          <ul key={item.movie_id}>
+            <li>{item.title}</li>
+          </ul>
+        );
+      })}
+      {bollywoodData?.map((item) => {
+        return (
+          <ul key={item.movie_id}>
+            <li>{item.title}</li>
           </ul>
         );
       })}
